c612bf9a5978d1652c8e0d247853e9fa601c3cb6
Balazs Marton <balazs.marton@prezi.com>
2021-05-13 22:43:19 +0200

    feature: load data from csv

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import pandas
 
+ALPHABET_FILE_PATH = 'nato_phonetic_alphabet.csv'
+
+
+def create_alphabet_from_file(filepath):
+    return pandas.read_csv(filepath)
+
+
 student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
@@ -21,8 +28,8 @@ for (index, row) in student_data_frame.iterrows():
 # Keyword Method with iterrows()
 # {new_key:new_value for (index, row) in df.iterrows()}
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
+# Create a dictionary in this format: {"A": "Alfa", "B": "Bravo"}
+nato_alphabet = create_alphabet_from_file(ALPHABET_FILE_PATH)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
