c994762ab97f132978797b979f77998238598b57
Balazs Marton <balazs.marton@prezi.com>
2021-05-13 22:37:35 +0200

    refact: hoist import to code start

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pandas
+
 student_dict = {
     "student": ["Angela", "James", "Lily"], 
     "score": [56, 76, 98]
@@ -8,7 +10,6 @@ for (key, value) in student_dict.items():
     #Access key and value
     pass
 
-import pandas
 student_data_frame = pandas.DataFrame(student_dict)
 
 #Loop through rows of a data frame
