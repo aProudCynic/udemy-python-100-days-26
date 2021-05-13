cceda515b686dbb73b0c9104890b364aba48ae02
Balazs Marton <balazs.marton@prezi.com>
2021-05-13 22:43:58 +0200

    refact: pep8 compatible comments

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ student_dict = {
     "score": [56, 76, 98]
 }
 
-#Looping through dictionaries:
+# Looping through dictionaries:
 for (key, value) in student_dict.items():
-    #Access key and value
+    # Access key and value
     pass
 
 student_data_frame = pandas.DataFrame(student_dict)
 
-#Loop through rows of a data frame
+# Loop through rows of a data frame
 for (index, row) in student_data_frame.iterrows():
-    #Access index and row
-    #Access row.student or row.score
+    # Access index and row
+    # Access row.student or row.score
     pass
 
 # Keyword Method with iterrows()
